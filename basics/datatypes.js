031b9ef2f4d5f83b050ddac6895097c730517a83
Developer01825345 <d01825345@gmail.com>
2025-07-09 05:48:42 +0000

    Add datatype file

---
+++ basics/datatypes.js
@@ -0,0 +1,10 @@
+let score = "10abc";
+
+console.log(typeof score)
+console.log(typeof(score));
+
+// Conversions
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
